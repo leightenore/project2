1d81c8afcedef8c296ab05c4a477ce0fa37f918d
ltenore <leightenore@gmail.com>
2019-02-27 21:25:24 -0700

    started database and user model

---
+++ models/user.js
@@ -0,0 +1,17 @@
+module.exports = function (sequelize, DataTypes) {
+    const User = sequelize.define("User", {
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        userpassword: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        destination_match: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    });
+    return User;
+};
