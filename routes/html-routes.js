3d3aa259990338b139e60be0704d93091bd4c066
Jenae <Jenaenay15@yahoo.com>
2019-03-02 10:02:01 -0700

    added partially finished routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,13 +1,11 @@
-var path = require("path");
+const path = require("path");
 
-// Routes
-// =============================================================
+
 module.exports = function(app) {
 
-  // Each of the below routes just handles the HTML page that the user gets sent to.
+  
   //the routes aren't complete because the file path hasn't been fully established
 
-  // index route loads view.html
   app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "../index.html"));
   });
@@ -16,8 +14,7 @@ module.exports = function(app) {
     res.sendFile(path.join(__dirname, "../survey.html"));
   });
 
-  app.get("/results", function
-  (req, res){
+  app.get("/results", function(req, res){
       res.sendFile(path.join(__dirname, "../results.html"))
   });
 }
