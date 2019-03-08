1cd4dc10cab1fc024cee20fb334d9cbe6e0924eb
Jenae <Jenaenay15@yahoo.com>
2019-03-07 20:33:38 -0700

    pushing so I can pull

--- models/destination.js
+++ models/destination.js
@@ -13,19 +13,19 @@ module.exports = function (sequelize, DataTypes) {
         price_point: {
             type: DataTypes.STRING,
             allowNull: false
-        },
-        image_one: {
-            type: DataTypes.STRING, 
-            allowNull: true
-        },
-        image_two: {
-            type: DataTypes.STRING, 
-            allowNull: true
-        },
-        image_three: {
-            type: DataTypes.STRING, 
-            allowNull: true
-        }
+        // },
+        // image_one: {
+        //     type: DataTypes.STRING, 
+        //     allowNull: true
+        // },
+        // image_two: {
+        //     type: DataTypes.STRING, 
+        //     allowNull: true
+        // },
+        // image_three: {
+        //     type: DataTypes.STRING, 
+        //     allowNull: true
+         }
     },{
         timestamps: false
     });
